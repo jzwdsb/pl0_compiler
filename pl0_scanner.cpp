17b8ce8804f83f74ed4b8ee9ae48fa912c9aa17e
jzwdsb <545373574@qq.com>
2017-09-09 21:44:26 +0800

    cpp version

--- pl0_scanner.cpp
+++ pl0_scanner.cpp
@@ -1,5 +1,26 @@
 #include <iostream>
 #include <algorithm>
 #include <regex>
-#include <
+#include <string>
+#include <sstream>
+
+using std::string;
+using std::stringstream;
+
+string digit        = R"([0-9])"
+
+string keywords     = R"(const|var|procedure|begin|end|if|then|while|do|read|call|write|writeln|module|int)"
+
+string constant     = R"([0-9]+)"
+
+string identfier    = R"([A-Za-z_][A-Za-Z_0-9]*)"
+
+string operator_tag = R"(\+|-|\*|\/|#|=|<|>|<>|:=|>=|<=)"
+
+string delimiter    = R"([,.;():])"
+
+string delim		= R"([ \n\t])"
+
+string whitespace	= R"([ \n\t]+)"
+
 
