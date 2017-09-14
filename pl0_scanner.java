60ada132570541442f52b62c67d0ee06899b7276
jzwdsb <545373574@qq.com>
2017-09-14 14:35:19 +0800

    modify format

--- pl0_scanner.java
+++ pl0_scanner.java
@@ -2,7 +2,6 @@ package com.manout;
 
 import javafx.util.Pair;
 import org.jetbrains.annotations.NotNull;
-
 import java.io.*;
 import java.util.LinkedList;
 import java.util.List;
@@ -10,65 +9,40 @@ import java.util.regex.*;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
-import static java.util.stream.Collectors.toList;
-
-public class Main {
-
-    private static String keywords     = "\\s*(const|var|procedure|begin|end|if|then|while|do|read|call|write|writeln|module|int)\\s*";
-
+public class pl0_scanner {
+    private static String keywords     = "\\s*(const|var|procedure|begin|end|if|then|while" +
+                                                "|do|read|call|write|writeln|module|int)\\s*";
     private static String constant     = "[0-9]+";
-
     private static String identifier    = "[A-Za-z_][A-Za-z_0-9]*";
-
     private static String operator     = "\\+|-|\\*|/|#|=|<|>|<>|:=|>=|<=";
-
     private static String delimiter    = "[,.;():]";
-
-    private static String separator = "((?<=:=)|(?=:=))|((?<=[+\\-*/#=<>,.;():])|(?=[+\\-*/#=<>./;():]))|\\s+";
     public static void main(String[] args) throws IOException {
 	    String filename = args[0];
         File file = new File(filename);
         BufferedReader fileReader = new BufferedReader(new FileReader(file));
         String line;
         List<Pair<String, Integer>> result = new LinkedList<>();
-
         while ((line = fileReader.readLine()) != null)
         {
-
-            Stream.of(line.split(separator)).filter(value -> !value.equals(""))
-                    .collect(Collectors.toList()).forEach(value -> result.add(matchType(value)));
+            Stream.of(line.split("((?<=\\W)+|(?=\\W)+)"))
+                    .collect(Collectors.toList())
+                    .forEach(value -> result.add(matchType(value)));
         }
-        result.forEach(value -> { if (value.getValue() != 0) System.out.println(value.getKey() + " : " + value.getValue().toString());});
-
+        result.forEach(value -> { if (value.getValue() != 0) System.out.println(value.getKey() +
+                                        "\t : \t" + value.getValue().toString());});
     }
-
     private static Pattern keywords_pattern   = Pattern.compile(keywords);
     private static Pattern constant_pattern   = Pattern.compile(constant);
     private static Pattern operator_pattern   = Pattern.compile(operator);
     private static Pattern delimiter_pattern  = Pattern.compile(delimiter);
     private static Pattern identifier_pattern = Pattern.compile(identifier);
-
     @NotNull
     private static Pair<String, Integer> matchType(String word) {
-        if (keywords_pattern.matcher(word).matches()) {
-            return new Pair<>(word, 1);
-        }
-        if (operator_pattern.matcher(word).matches())
-        {
-            return new Pair<>(word, 2);
-        }
-        if (delimiter_pattern.matcher(word).matches())
-        {
-            return new Pair<>(word, 3);
-        }
-        if (constant_pattern.matcher(word).matches())
-        {
-            return new Pair<>(word, 4);
-        }
-        if (identifier_pattern.matcher(word).matches())
-        {
-            return new Pair<>(word, 5);
-        }
+        if (keywords_pattern.matcher(word).matches()) { return new Pair<>(word, 1); }
+        if (operator_pattern.matcher(word).matches()) { return new Pair<>(word, 2); }
+        if (delimiter_pattern.matcher(word).matches()){ return new Pair<>(word, 3); }
+        if (constant_pattern.matcher(word).matches()) { return new Pair<>(word, 4); }
+        if (identifier_pattern.matcher(word).matches()){ return new Pair<>(word, 5);}
         return new Pair<>("",0);
     }
 }
