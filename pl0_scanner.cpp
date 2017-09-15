9f0338eecb9592fc518042104b266cf5860c08d0
jzwdsb <545373574@qq.com>
2017-09-15 22:09:24 +0800

    almost done, write cpp code is a tough experience

--- pl0_scanner.cpp
+++ pl0_scanner.cpp
@@ -1,48 +1,51 @@
 #include <iostream>
 #include <algorithm>
 #include <regex>
+#include <vector>
 #include <string>
-#include <sstream>
+#include <fstream>
+#include <boost/algorithm/string.hpp>
+#include <boost/algorithm/string/split.hpp>
+#include <boost/algorithm/string/classification.hpp>
+
 
 using std::string;
 using std::stringstream;
 using std::regex;
 using std::regex_match;
-
+using std::vector;
+using std::for_each;
 using std::cin;
 using std::cout;
-
 /*
  * c++ 版本的词法分析器
  */
 
-const string digit        = R"([0-9])"
+const string digit        = R"([0-9])";
 
 const string keywords     = R"(const|var|procedure|begin|end|if|then"
-								"|while|do|read|call|write|writeln|module|int)"
+								"|while|do|read|call|write|writeln|module|int)";
 
-const string constant     = R"([0-9]+)"
+const string constant     = R"([0-9]+)";
 
-const string identfier    = R"([A-Za-z_][A-Za-Z_0-9]*)"
+const string identfier    = R"([A-Za-z_][A-Za-Z_0-9]*)";
 
-const string operator_tag = R"(\+|-|\*|\/|#|=|<|>|<>|:=|>=|<=)"
+const string operator_tag = R"(\+|-|\*|\/|#|=|<|>|<>|:=|>=|<=)";
 
-const string delimiter    = R"([,.;():])"
+const string delimiter    = R"([,.;():])";
 
-const string delim		= R"([ \n\t])"
+const string delim		= R"([ \n\t])";
 
-const string whitespace	= R"([ \n\t]+)"
+const string whitespace	= R"([ \n\t]+)";
 
 enum word_type
 {
-	digit_,
 	keywords_,
-	constant_,
-	identfier_,
 	operator_tag_,
 	delimiter_,
-	delim_,
-	whitespace_
+	constant_,
+	identfier_,
+	error
 };
 
 regex digit_regex(digit);
@@ -54,31 +57,38 @@ regex delimiter_regex(delimiter);
 regex whitespace_regex(whitespace);
 
 
-bool check_is_separator(char);
 
-bool get_word(stringstream& ss, string& str)
+word_type get_word_type(const string& word)
 {
-	string tmp;
-	const static string separator = operator_tag + delimiter;
-	if(not  ss << tmp)
-	{
-		return false;
-	}
-	if (regex_search(identfier, )  tmp.find_first_of(separator) not_eq string::npos)
-	{
-		return false;
-	}
+	if (regex_match(word, keywords_regex))
+		return word_type::keywords_;
+	if(regex_match(word, operator_tag_regex))
+		return word_type::operator_tag_;
+	if(regex_match(word, delimiter_regex))
+		return word_type::delimiter_;
+	if(regex_match(word, constant_regex))
+		return word_type::constant_;
+	if(regex_match(word, identfier_regex))
+		return word_type::identfier_;
 	
+	return word_type::error;
 }
-
 int main(int argc, char* argv[], char* env[])
 {
-	stringstream ss(cin);
-	string current_word;
-	while ()
+	std::fstream filestream(argv[1], std::ios_base::in);
+	const string separator = "+-*/,.;:()<>:=";
+	string line;
+	vector<string> words;
+	vector<std::pair<string, word_type>> result;
+	while (std::getline(filestream, line))
 	{
-		if ()
+		boost::split(words, line, boost::is_any_of(separator));
+		std::for_each(words.begin(), words.end(), [&](const string& word){
+					result.emplace_back(word, get_word_type(word));
+				});
 	}
-
+	for_each(result.begin(), result.end(), [](const std::pair<string, word_type> word )
+										{ printf("%s\t:\t%d", word.first.c_str(), word.second); });
+	return 0;
 }
 
