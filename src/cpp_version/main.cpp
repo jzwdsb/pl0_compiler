f0f4916fc8bb59b92cfcca34657e75d4e3573595
jzwdsb <manout960920@outlook.com>
2018-01-10 22:34:39 +0800

    add interact

--- src/cpp_version/main.cpp
+++ src/cpp_version/main.cpp
@@ -3,17 +3,23 @@
 //
 
 #include <memory>
+#include <iostream>
 
 #include "pl0.h"
 #include "Scanner.h"
 #include "Lexer.h"
 
-std::unique_ptr<Scanner> scanner( new  Scanner("/home/manout/pl0_compiler/demo.pl0"));
-std::unique_ptr<Lexer>   lexer ( new Lexer(scanner.get()));
+std::unique_ptr<Scanner> scanner;
+std::unique_ptr<Lexer>   lexer;
 
 /**	主程序的入口*/
 int main ()
 {
+	std::string filename;
+	std::cout << "please input the pl0 source file, full path" << std::endl;
+	std::cin >> filename;
+	scanner.reset(new Scanner(filename));
+	lexer.reset(new Lexer(scanner.get()));
 	program();
 	show_code();
 	save_code();
